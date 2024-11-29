2ec597d64a140f5498517b7f057bca48fd33e1f4
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2024-11-29 17:37:16 +0200

    data for lab

---
+++ lab2/src/myData/myData.jsx
@@ -0,0 +1,19 @@
+const images = [
+    { id: "id_1", src: "../public/img/image_1.jpg", placeholder: "placeholder 1", className: "lazy-loaded-image" },
+    { id: "id_2", src: "../public/img/image_2.jpg", placeholder: "placeholder 2", className: "lazy-loaded-image" },
+    { id: "id_3", src: "../public/img/image_3.jpg", placeholder: "placeholder 3", className: "lazy-loaded-image" },
+
+    { id: "id_4", src: "../public/img/image_1.jpg", placeholder: "placeholder 4", className: "lazy-loaded-image" },
+    { id: "id_5", src: "../public/img/image_2.jpg", placeholder: "placeholder 5", className: "lazy-loaded-image" },
+    { id: "id_6", src: "../public/img/image_3.jpg", placeholder: "placeholder 6", className: "lazy-loaded-image" },
+
+    { id: "id_7", src: "../public/img/image_1.jpg", placeholder: "placeholder 7", className: "lazy-loaded-image" },
+    { id: "id_8", src: "../public/img/image_2.jpg", placeholder: "placeholder 8", className: "lazy-loaded-image" },
+    { id: "id_9", src: "../public/img/image_3.jpg", placeholder: "placeholder 9", className: "lazy-loaded-image" },
+
+    { id: "id_10", src: "../public/img/image_1.jpg", placeholder: "placeholder 10", className: "lazy-loaded-image" },
+    { id: "id_11", src: "../public/img/image_2.jpg", placeholder: "placeholder 11", className: "lazy-loaded-image" },
+    { id: "id_12", src: "../public/img/image_3.jpg", placeholder: "placeholder 12", className: "lazy-loaded-image" }
+]
+
+export default images
